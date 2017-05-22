13f9d3c94f5b7230ba523c96502525471316e5ff
PAULUS GUNADI <paulus@sg.ibm.com>
2017-05-22 17:47:15 +0800

    Create test_sms.sh

---
+++ samples/test_sms.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export JAVA_HOME=/opt/ibm/ibm-java-x86_64-60
+export CLASS_PATH=$(echo lib/*.jar | tr ' ' ':')
+SMPP_HOST="smpp_server1,smpp_server1_alternate"
+SMPP_PORT="smpp_server1_service_port"
+SMPP_USER="smpp_server1_user_name"
+SMPP_PWD="smpp_server1_user_password"
+SMPP_HOST2="smpp_server2,smpp_server2_alternate"
+SMPP_PORT2="smpp_server2_port"
+SMPP_USER2="smpp_server2_user_name"
+SMPP_PWD2="smpp_server2_user_password"
+SMPP_SENDER="smpp_server_number_or_name"
+SMPP_RECIPIENTS=$1
+SMPP_TEXT=$2
+$JAVA_HOME/bin/java -cp $CLASS_PATH com.ibm.smpp.client.utils.SMPPClient160 $SMPP_HOST $SMPP_PORT $SMPP_USER $SMPP_PWD $SMPP_HOST2 $SMPP_PORT2 $SMPP_USER2 $SMPP_PWD2 $SMPP_SENDER "$SMPP_RECIPIENTS" "$SMPP_TEXT"
